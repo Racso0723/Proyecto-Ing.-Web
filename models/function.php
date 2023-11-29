c2b6a67c15763384f470b7f0c64633e73fb18cad
Racso0723 <132211397+Racso0723@users.noreply.github.com>
2023-11-29 16:51:42 -0500

    Add files via upload

---
+++ models/function.php
@@ -0,0 +1,105 @@
+<?php
+
+function dbConnect()
+{
+    $connection = mysqli_connect('localhost', 'root', '', 'reservaya');
+
+    if (!$connection) {
+        die("Conexion a la Base de Datos ha fallado: " . mysqli_connect_error());
+    }
+
+    return $connection;
+}
+
+function getTipoComida()
+{
+    $mysqli = dbConnect();
+    $result = $mysqli->query("SELECT DISTINCT tipo_comida FROM restaurantes");
+    $comidas = array();
+    while ($row = $result->fetch_assoc()) {
+        $comidas[] = $row['tipo_comida'];
+    }
+    return $comidas;
+}
+
+function getTipolocal()
+{
+    $mysqli = dbConnect();
+    $result = $mysqli->query("SELECT DISTINCT tipo_local FROM restaurantes");
+    $locales = array();
+    while ($row = $result->fetch_assoc()) {
+        $locales[] = $row['tipo_local'];
+    }
+    return $locales;
+}
+
+function getCosto()
+{
+    $mysqli = dbConnect();
+    $result = $mysqli->query("SELECT DISTINCT costo FROM restaurantes");
+    $costos = array();
+    while ($row = $result->fetch_assoc()) {
+        $costos[] = $row['costo'];
+    }
+    return $costos;
+}
+
+function getFacilidades()
+{
+    $mysqli = dbConnect();
+    $result = $mysqli->query("SELECT DISTINCT facilidades FROM restaurantes");
+    $facilidades = array();
+    while ($row = $result->fetch_assoc()) {
+        $facilidades[] = $row['facilidades'];
+    }
+    return $facilidades;
+}
+
+function getRestaurantesPorNombre($nombre) {
+    $mysqli = dbConnect();
+    $nombre = $mysqli->real_escape_string($nombre); // Evitar inyección SQL
+
+    $query = "SELECT * FROM restaurantes WHERE nombre LIKE '%$nombre%'";
+    $result = $mysqli->query($query);
+
+    if ($result) {
+        return $result->fetch_all(MYSQLI_ASSOC);
+    } else {
+        return []; // Manejo de errores según sea necesario
+    }
+}
+
+
+function getBusquedaRestaurantes($int)
+{
+    $mysqli = dbConnect();
+    $result = $mysqli->query("SELECT * FROM restaurantes ORDER BY rand() LIMIT $int");
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    return $data;
+}
+
+function getFilteredRestaurants($selectedComidas, $selectedLocales, $selectedCostos, $selectedFacilidades) {
+    $mysqli = dbConnect();
+    $comidas = implode("','", $selectedComidas);
+    $locales = implode("','", $selectedLocales);
+    $costos = implode("','", $selectedCostos);
+    $facilidades = implode("','", $selectedFacilidades);
+
+    $query = "SELECT * FROM restaurantes WHERE 
+        (tipo_comida IN ('$comidas') OR 
+        tipo_local IN ('$locales') OR 
+        costo IN ('$costos') OR 
+        facilidades IN ('$facilidades'))";
+
+    $result = $mysqli->query($query);
+
+    if ($result) {
+        return $result->fetch_all(MYSQLI_ASSOC);
+    } else {
+        return []; // Manejo de errores según sea necesario
+    }
+}
+
+
